9272a82d916023aca10490b492af40b20060a35b
zronowa17-maker <zronowa17@gmail.com>
2025-09-18 18:40:21 +0800

    create ui

--- BasicThread/Form1.Designer.cs
+++ BasicThread/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bntRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-Before starting thread-";
+            // 
+            // bntRun
+            // 
+            this.bntRun.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bntRun.Location = new System.Drawing.Point(185, 181);
+            this.bntRun.Name = "bntRun";
+            this.bntRun.Size = new System.Drawing.Size(80, 31);
+            this.bntRun.TabIndex = 1;
+            this.bntRun.Text = "Run";
+            this.bntRun.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(481, 300);
+            this.Controls.Add(this.bntRun);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bntRun;
     }
 }
 
