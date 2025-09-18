52fb6d5ace5448dca614b294cda1593be9ef1ee8
zronowa17-maker <zronowa17@gmail.com>
2025-09-18 19:05:01 +0800

    adding code loop

--- BasicThread/MyThreadClass.cs
+++ BasicThread/MyThreadClass.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace BasicThread
 {
-    internal class MyThreadClass
+    public class MyThreadClass
     {
+        public static void Thread1()
+        {
+            for (int loopcount = 0; loopcount <= 5; loopcount++)
+            {
+                Console.WriteLine("Name Of Thread: " + Thread.CurrentThread.Name + " = " + loopcount);
+                Thread.Sleep(1500);
+            }
+        }
     }
 }
+         
+            
+  
+
+
