39eaeb195c1951a7357bc4b809879c47d852f768
zronowa17-maker <zronowa17@gmail.com>
2025-09-18 19:42:45 +0800

    adjustment

--- BasicThread/FrmBasicThread.Designer.cs
+++ BasicThread/FrmBasicThread.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmBasicThread));
             this.label1 = new System.Windows.Forms.Label();
             this.bntRun = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -35,8 +36,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(123, 64);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(138, 156);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(199, 24);
             this.label1.TabIndex = 0;
@@ -45,7 +48,7 @@
             // bntRun
             // 
             this.bntRun.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.bntRun.Location = new System.Drawing.Point(185, 181);
+            this.bntRun.Location = new System.Drawing.Point(199, 183);
             this.bntRun.Name = "bntRun";
             this.bntRun.Size = new System.Drawing.Size(80, 31);
             this.bntRun.TabIndex = 1;
@@ -53,15 +56,18 @@
             this.bntRun.UseVisualStyleBackColor = true;
             this.bntRun.Click += new System.EventHandler(this.bntRun_Click);
             // 
-            // Form1
+            // FrmBasicThread
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(481, 300);
             this.Controls.Add(this.bntRun);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Name = "FrmBasicThread";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
